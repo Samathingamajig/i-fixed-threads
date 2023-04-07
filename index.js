c978ff1ea4a6acbcaddf8b42a5ca9f88c2764b02
Samuel Gunter <samathingamajig@gmail.com>
2023-04-07 02:48:33 -0500

    save tweets to localStorage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 const getTweetButton = () => document.querySelector('[data-testid="tweetButton"]');
 const getAddTweetButton = () => document.querySelector('[aria-label="Add Tweet"]');
+const getTweetDrafts = () => document.querySelectorAll('[role="dialog"] [aria-label="Tweet text"]');
 const rightClickHandler = (e) => {
     var _a;
     if (!((_a = getTweetButton()) === null || _a === void 0 ? void 0 : _a.contains(e.target)))
@@ -13,10 +14,14 @@ const rightClickHandler = (e) => {
     else {
         // save tweets
         console.log("save tweets");
+        const draftElements = Array.from(getTweetDrafts());
+        const draftContents = draftElements.map((draft) => draft.innerText.trim().replaceAll("\n\n", "\n")).filter(Boolean);
+        localStorage.setItem(":sgunter:thread", JSON.stringify(draftContents));
     }
 };
 window.addEventListener("contextmenu", rightClickHandler);
 window.s = {
     getAddTweetButton,
     getTweetButton,
+    getTweetDrafts,
 };
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 const getTweetButton = () => document.querySelector<HTMLDivElement>('[data-testid="tweetButton"]');
 const getAddTweetButton = () => document.querySelector<HTMLDivElement>('[aria-label="Add Tweet"]');
+const getTweetDrafts = () => document.querySelectorAll<HTMLDivElement>('[role="dialog"] [aria-label="Tweet text"]');
 
 const rightClickHandler = (e: MouseEvent) => {
   if (!getTweetButton()?.contains(e.target as Node)) return;
@@ -12,6 +13,9 @@ const rightClickHandler = (e: MouseEvent) => {
   } else {
     // save tweets
     console.log("save tweets");
+    const draftElements = Array.from(getTweetDrafts());
+    const draftContents = draftElements.map((draft) => draft.innerText.trim().replaceAll("\n\n", "\n")).filter(Boolean);
+    localStorage.setItem(":sgunter:thread", JSON.stringify(draftContents));
   }
 };
 
@@ -20,4 +24,5 @@ window.addEventListener("contextmenu", rightClickHandler);
 (window as any).s = {
   getAddTweetButton,
   getTweetButton,
+  getTweetDrafts,
 };
